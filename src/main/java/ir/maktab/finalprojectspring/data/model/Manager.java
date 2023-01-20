9d299328efaad4bb759d8d4a5c9f7ba05e9088c7
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-20 21:23:01 +0330

    add Manager.java class

---
+++ src/main/java/ir/maktab/finalprojectspring/data/model/Manager.java
@@ -0,0 +1,29 @@
+package ir.maktab.finalprojectspring.data.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class Manager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String username;
+
+    @Column(nullable = false)
+    String password;
+
+}
