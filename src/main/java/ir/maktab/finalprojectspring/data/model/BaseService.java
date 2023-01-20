f8d521882d85fd550f046b624decd0e05ebb5f20
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-20 21:17:59 +0330

    add BaseService.java class

---
+++ src/main/java/ir/maktab/finalprojectspring/data/model/BaseService.java
@@ -0,0 +1,29 @@
+package ir.maktab.finalprojectspring.data.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+
+public class BaseService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+}
