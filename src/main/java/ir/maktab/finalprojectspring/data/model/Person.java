40bc534973e2cdd0402c69329923e5b6341680e9
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-20 21:10:55 +0330

    add Person.java class

---
+++ src/main/java/ir/maktab/finalprojectspring/data/model/Person.java
@@ -0,0 +1,46 @@
+package ir.maktab.finalprojectspring.data.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PROTECTED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    Double credit;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    String familyName;
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    String username;
+
+    @CreationTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    Date date;
+}
