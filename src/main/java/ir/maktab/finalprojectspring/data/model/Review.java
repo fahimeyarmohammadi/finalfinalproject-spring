aa24a0172f95b7d0fc64af5dd5c661e41fd89713
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-20 21:26:08 +0330

    add Review.java class

---
+++ src/main/java/ir/maktab/finalprojectspring/data/model/Review.java
@@ -0,0 +1,28 @@
+package ir.maktab.finalprojectspring.data.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    int score;
+
+    String comment;
+
+}
