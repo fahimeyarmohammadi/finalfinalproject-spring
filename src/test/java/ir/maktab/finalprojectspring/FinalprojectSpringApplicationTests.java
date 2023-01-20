0b3f7bc01794b954bb0f701a6e135accad81aaac
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-20 20:57:39 +0330

    Initial commit

---
+++ src/test/java/ir/maktab/finalprojectspring/FinalprojectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktab.finalprojectspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
