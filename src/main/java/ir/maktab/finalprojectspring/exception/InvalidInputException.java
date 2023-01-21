7a6b0769d43a37c4afce0bc0eefbbc72aee5bae0
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-21 16:18:28 +0330

    add InvalidInputException.java class

---
+++ src/main/java/ir/maktab/finalprojectspring/exception/InvalidInputException.java
@@ -0,0 +1,8 @@
+package ir.maktab.finalprojectspring.exception;
+
+public class InvalidInputException extends Exception{
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
