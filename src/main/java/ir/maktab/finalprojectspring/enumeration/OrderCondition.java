67e447397e96d6eee2950daa6e4cddd630192655
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-21 09:56:08 +0330

    rename ORDERCONDITION class to OrderCondition

---
+++ src/main/java/ir/maktab/finalprojectspring/enumeration/OrderCondition.java
@@ -0,0 +1,17 @@
+package ir.maktab.finalprojectspring.enumeration;
+
+public enum OrderCondition {
+
+    WAITING_EXPERT_SUGGESTION,
+
+    WAITING_EXPERT_SELECTION,
+
+    WAITING_FOR_EXPERT_COMING,
+
+    STARTED,
+
+    DONE,
+
+    PAID
+
+}
