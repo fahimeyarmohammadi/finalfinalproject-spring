1ecb89c6e81e7aba5b24f1680716dae11b2f6e84
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-21 15:39:14 +0330

    add SubServiceRepository and SubServiceService methods

--- src/main/java/ir/maktab/finalprojectspring/data/repository/SubServiceRepository.java
+++ src/main/java/ir/maktab/finalprojectspring/data/repository/SubServiceRepository.java
@@ -2,6 +2,9 @@ package ir.maktab.finalprojectspring.data.repository;
 
 import ir.maktab.finalprojectspring.data.model.SubService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +12,16 @@ import java.util.Optional;
 
 @Repository
 public interface SubServiceRepository extends JpaRepository<SubService,Long> {
+
     Optional<SubService> findBySubName(String name);
+
     List<SubService> findAllByBaseService_Name(String name);
+
+    @Modifying
+    @Query("update SubService s SET s.basePrice =:basePrice WHERE s.subName =:subName")
+    void updateSubServiceBasePrice(@Param("subName") String subName, @Param("basePrice") Double basePrice);
+
+    @Modifying
+    @Query("update SubService s SET s.description =:description WHERE s.subName =:subName")
+    void updateSubServiceDescription(@Param("subName") String subName, @Param("description") String description);
 }
--- src/main/java/ir/maktab/finalprojectspring/service/SubServiceService.java
+++ src/main/java/ir/maktab/finalprojectspring/service/SubServiceService.java
@@ -1,4 +1,24 @@
 package ir.maktab.finalprojectspring.service;
 
+import ir.maktab.finalprojectspring.data.model.SubService;
+import ir.maktab.finalprojectspring.exception.NotFoundException;
+import ir.maktab.finalprojectspring.exception.ObjectExistException;
+
+import java.util.List;
+
 public interface SubServiceService {
+
+    void addSubService(SubService subService) throws NotFoundException, ObjectExistException;
+
+    List<SubService> getAllSubService();
+
+    List<SubService> getAllSubServiceInBaseService(String baseServiceName) throws NotFoundException;
+
+    SubService getSubServiceByName(String subName) throws NotFoundException;
+
+    void changeSubServiceBasePrice(String subName, Double newPrice);
+
+    void changeSubServiceDescription(String subName, String newDescription);
+
+
 }
