5844a506f6274eda209479f0e025f4e331da0c83
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-21 18:18:42 +0330

    add ExpertServiceIMPL.java class

---
+++ src/main/java/ir/maktab/finalprojectspring/service/ExpertServiceIMPL.java
@@ -0,0 +1,10 @@
+package ir.maktab.finalprojectspring.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExpertServiceIMPL implements ExpertService{
+
+}
