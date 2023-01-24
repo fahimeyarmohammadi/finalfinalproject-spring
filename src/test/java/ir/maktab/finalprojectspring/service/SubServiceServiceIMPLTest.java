37fc5a1490c4a813c036de966b30ae2081e48bb4
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-24 15:15:48 +0330

    add SubServiceServiceIMPLTest.java

---
+++ src/test/java/ir/maktab/finalprojectspring/service/SubServiceServiceIMPLTest.java
@@ -0,0 +1,32 @@
+package ir.maktab.finalprojectspring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubServiceServiceIMPLTest {
+
+    @Test
+    void addSubService() {
+    }
+
+    @Test
+    void getAllSubService() {
+    }
+
+    @Test
+    void getAllSubServiceInBaseService() {
+    }
+
+    @Test
+    void getSubServiceByName() {
+    }
+
+    @Test
+    void changeSubServiceBasePrice() {
+    }
+
+    @Test
+    void changeSubServiceDescription() {
+    }
+}
