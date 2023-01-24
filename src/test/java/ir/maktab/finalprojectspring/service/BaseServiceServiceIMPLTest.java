0d65c194a4874c4b6f3fb9f68503f928498e068c
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-24 11:07:46 +0330

    add aseServiceIMPLTest.java

--- src/test/java/ir/maktab/finalprojectspring/service/BaseServiceServiceIMPLTest.java
+++ src/test/java/ir/maktab/finalprojectspring/service/BaseServiceServiceIMPLTest.java
@@ -1,7 +1,20 @@
 package ir.maktab.finalprojectspring.service;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BaseServiceServiceIMPLTest {
 
+    @Test
+    void addBaseService() {
+    }
+
+    @Test
+    void getAllBaseService() {
+    }
+
+    @Test
+    void getBaseServiceByName() {
+    }
 }
