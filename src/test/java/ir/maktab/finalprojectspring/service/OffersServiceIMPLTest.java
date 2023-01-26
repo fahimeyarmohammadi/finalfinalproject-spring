d4ab836d75b2fe8b5737400dd06f0c5c312bda15
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-26 18:37:12 +0330

    add OffersServiceIMPLTest.java

---
+++ src/test/java/ir/maktab/finalprojectspring/service/OffersServiceIMPLTest.java
@@ -0,0 +1,20 @@
+package ir.maktab.finalprojectspring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OffersServiceIMPLTest {
+
+    @Test
+    void addOffers() {
+    }
+
+    @Test
+    void getOffersListOrderedByPrice() {
+    }
+
+    @Test
+    void getOffersListOrderedByExpertScore() {
+    }
+}
