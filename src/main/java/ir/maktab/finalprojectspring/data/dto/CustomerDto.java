f923bf81af42a72260d576e2b77cae6275ca2b88
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-30 20:57:47 +0330

    creat CustomerDto.java class and CustomerMapper.java interface

---
+++ src/main/java/ir/maktab/finalprojectspring/data/dto/CustomerDto.java
@@ -0,0 +1,18 @@
+package ir.maktab.finalprojectspring.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CustomerDto {
+
+    private String name;
+
+    private String familyName;
+
+    private String username;
+
+    private String password;
+
+}
---
+++ src/main/java/ir/maktab/finalprojectspring/mapper/CustomerMapper.java
@@ -0,0 +1,15 @@
+package ir.maktab.finalprojectspring.mapper;
+
+import ir.maktab.finalprojectspring.data.dto.CustomerDto;
+import ir.maktab.finalprojectspring.data.model.Customer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CustomerMapper {
+
+    CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
+
+    Customer dtoToModel(CustomerDto userDto);
+
+}
