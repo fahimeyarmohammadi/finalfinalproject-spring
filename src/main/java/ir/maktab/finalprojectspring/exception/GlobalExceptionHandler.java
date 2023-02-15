ce43935adc035973b5ccd9ac1dd0318510cf38ff
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-02-15 15:34:59 +0330

    finish phase3-update

--- src/main/java/ir/maktab/finalprojectspring/exception/GlobalExceptionHandler.java
+++ src/main/java/ir/maktab/finalprojectspring/exception/GlobalExceptionHandler.java
@@ -1,11 +1,9 @@
 package ir.maktab.finalprojectspring.exception;
 
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
